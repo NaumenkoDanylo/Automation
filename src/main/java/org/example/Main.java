4486182c8ac30f6b3a7083e6ede76df241e6cdf3
NaumenkoDanylo <crucifixflame@gmail.com>
2023-11-03 16:48:24 +0200

    HW 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Home\\IdeaProjects\\Automation\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com.ua/");
+        System.out.println("Page open successfully");
+    }
+}
